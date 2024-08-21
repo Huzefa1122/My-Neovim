f5094267a9e6514c9e803f61c890690ffa2698b5
huzefa1122 <muhammadhuzefa2003@gmail.com>
2024-08-21 20:11:45 +0500

    Updated setup

--- init.lua
+++ init.lua
@@ -6,38 +6,37 @@ vim.g.mapleader = " "
 -- Install lazy.nvim if it's not installed
 local lazypath = vim.fn.stdpath("config") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugin configuration
 require("lazy").setup({
-  "nvim-lua/plenary.nvim",
-  "nvim-telescope/telescope.nvim",
-  "nvim-treesitter/nvim-treesitter",
-  "neovim/nvim-lspconfig",
-  "mg979/vim-visual-multi",
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-nvim-lsp",
-  "nvim-lualine/lualine.nvim",
-  "gruvbox-community/gruvbox",
-  "tpope/vim-commentary",
-  "windwp/nvim-autopairs",
-  "navarasu/onedark.nvim",
-  "nvim-tree/nvim-tree.lua",      -- Add nvim-tree plugin
-  "nvim-tree/nvim-web-devicons",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "neovim/nvim-lspconfig",
+    "mg979/vim-visual-multi",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "nvim-lualine/lualine.nvim",
+    "gruvbox-community/gruvbox",
+    "tpope/vim-commentary",
+    "windwp/nvim-autopairs",
+    "navarasu/onedark.nvim",
+    "nvim-tree/nvim-tree.lua", -- Add nvim-tree plugin
+    "nvim-tree/nvim-web-devicons",
 
-
-  -- Add Mason and Mason-LSPconfig
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  -- Adding AutoImport 
+    -- Add Mason and Mason-LSPconfig
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    -- Adding AutoImport
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
@@ -45,11 +44,10 @@ require("lazy").setup({
     "onsails/lspkind-nvim",
     "akinsho/toggleterm.nvim",
     "jose-elias-alvarez/nvim-lsp-ts-utils",
-    "mg979/vim-visual-multi",
 
     -- Formatter
-"jose-elias-alvarez/null-ls.nvim"
-    })
+    "jose-elias-alvarez/null-ls.nvim"
+})
 
 -- Basic settings
 vim.opt.number = true
@@ -76,7 +74,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
 -- Setup Mason
 require("mason").setup()
--- Formatter  
+-- Formatter
 local null_ls = require("null-ls")
 
 -- Configure null-ls with Prettier
@@ -103,23 +101,32 @@ require("mason-lspconfig").setup({
 -- Multi Cursor Setup
 
 vim.g.VM_maps = {
-    ['Find Under'] = '<C-A-Down>',  -- Start multicursor selection
-    ['Find Subword Under'] = '<C-n>',  -- Start multicursor selection for subwords
-    ['Select All'] = '<C-F2>',  -- Select all occurrences
-    ['Skip Region'] = '<A>',  -- Skip the current selection
+    ['Find Under'] = '<C-A-Down>',    -- Start multicursor selection
+    ['Find Subword Under'] = '<C-A-n>', -- Start multicursor selection for subwords
+    ['Select All'] = '<C-F2>',        -- Select all occurrences
+    ['Skip Region'] = '<A>',          -- Skip the current selection
 }
 
 -- LSP Configuration
 local lspconfig = require('lspconfig')
 
 -- Language servers setup
-lspconfig.gopls.setup{}
-lspconfig.clangd.setup{}
-lspconfig.tsserver.setup{}
-lspconfig.svelte.setup{}
-lspconfig.html.setup{}
-lspconfig.cssls.setup{}
-lspconfig.lua_ls.setup{
+lspconfig.gopls.setup {}
+lspconfig.clangd.setup {}
+lspconfig.tsserver.setup {
+    on_attach = function(client)
+        local ts_utils = require("nvim-lsp-ts-utils")
+        ts_utils.setup({
+            auto_inlay_hints = true,
+            inlay_hints_highlight = "Comment",
+        })
+        ts_utils.setup_client(client)
+    end,
+}
+lspconfig.svelte.setup {}
+lspconfig.html.setup {}
+lspconfig.cssls.setup {}
+lspconfig.lua_ls.setup {
     settings = {
         Lua = {
             runtime = {
@@ -127,7 +134,7 @@ lspconfig.lua_ls.setup{
                 path = vim.split(package.path, ';')
             },
             diagnostics = {
-                globals = {'vim'},
+                globals = { 'vim' },
             },
             workspace = {
                 library = vim.api.nvim_get_runtime_file("", true),
@@ -142,72 +149,61 @@ lspconfig.lua_ls.setup{
 
 -- Autocompletion
 -- LSP Setup
-local lspconfig = require('lspconfig')
 local cmp = require('cmp')
 local lspkind = require('lspkind')
 
 -- nvim-cmp setup for auto-imports
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = {
-    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-  },
-  formatting = {
-    format = lspkind.cmp_format({ with_text = true, maxwidth = 50 })
-  },
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end,
+    },
+    mapping = {
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+    },
+    formatting = {
+        format = lspkind.cmp_format({ with_text = true, maxwidth = 50 })
+    },
 })
 
 -- LSP configuration for TypeScript/JavaScript with auto-imports
-lspconfig.tsserver.setup({
-    on_attach = function(client)
-        local ts_utils = require("nvim-lsp-ts-utils")
-        ts_utils.setup({
-            auto_inlay_hints = true,
-            inlay_hints_highlight = "Comment",
-        })
-        ts_utils.setup_client(client)
-    end,
-})
 
 -- Treesitter (Better Syntax Highlighting)
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "python", "javascript", "html", "css", "go", "svelte" },
-  highlight = {
-    enable = true,
-  },
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "c", "lua", "python", "javascript", "html", "css", "go", "svelte" },
+    highlight = {
+        enable = true,
+    },
 }
 
 -- Auto-pairs (Automatic closing of brackets, quotes, etc.)
-require('nvim-autopairs').setup{}
+require('nvim-autopairs').setup {}
 -- nvim-tree configuration
 require("nvim-tree").setup({
-  view = {
-    side = "right",  -- Open the file explorer on the right
-    width = 30,
-  },
-  renderer = {
-    icons = {
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = true,
-      },
+    view = {
+        side = "right", -- Open the file explorer on the right
+        width = 30,
     },
-  },
-  filters = {
-    dotfiles = true,  -- Show hidden files (set to true to hide dotfiles)
-  },
+    renderer = {
+        icons = {
+            show = {
+                file = true,
+                folder = true,
+                folder_arrow = true,
+                git = true,
+            },
+        },
+    },
+    filters = {
+        dotfiles = false, -- Show hidden files (set to true to hide dotfiles)
+    },
     actions = {
         open_file = {
             quit_on_open = true,
@@ -217,12 +213,12 @@ require("nvim-tree").setup({
 
 -- ToggleTerm setup
 require("toggleterm").setup({
-    size = 20,  -- Height of the terminal window
-    open_mapping = [[<C-\>]],  -- Keybinding to toggle terminal
-    direction = 'horizontal',  -- Open terminal horizontally
-    shading_factor = 2,  -- Darken the terminal background
-    close_on_exit = true,  -- Close terminal when process exits
-    shell = vim.o.shell,  -- Use default shell
+    size = 15,                -- Height of the terminal window
+    open_mapping = [[<C-\>]], -- Keybinding to toggle terminal
+    direction = 'horizontal', -- Open terminal horizontally
+    shading_factor = 2,       -- Darken the terminal background
+    close_on_exit = true,     -- Close terminal when process exits
+    shell = vim.o.shell,      -- Use default shell
 })
 -- Function to format the current buffer
 local function lsp_formatting(bufnr)
@@ -267,4 +263,3 @@ vim.keymap.set('n', '<C-e>', ':NvimTreeToggle<CR>')
 -- Keybinding to toggle the terminal
 vim.keymap.set('n', '<C-\\>', ':ToggleTerm<CR>')
 vim.api.nvim_set_keymap('n', '<A-f>', ':lua vim.lsp.buf.format({ async = true })<CR>', { noremap = true, silent = true })
-
